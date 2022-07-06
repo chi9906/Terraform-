a9ede45063751b57017c225c96c106019593d88a
Chiderah <chiderah96@gmail.com>
2022-07-06 12:30:35 -0500

    set changes

--- main.tf
+++ main.tf
@@ -30,20 +30,17 @@ tags = {
 } }
 #Deploy the public subnets
 resource "aws_subnet" "public_subnets" {
-for_each
-vpc_id
-cidr_block
-= var.public_subnets
-= aws_vpc.vpc.id
-= cidrsubnet(var.vpc_cidr, 8, each.value + 100)
-availability_zone       = tolist(data.aws_availability_zones.available.
-   names)[each.value]
+for_each    = var.public_subnetsvpc_id
+vpc_id      = aws_vpc.vpc.id
+cidr_block  = cidrsubnet(var.vpc_cidr, 8, each.value + 100)
+availability_zone = =tolist(data.aws_availability_zones.available.   names)[each.value]
 map_public_ip_on_launch = true
 tags = {
   Name      = each.key
 Terraform = "true"
   }
 }
+
 #Create route tables for public and private subnets resource "aws_route_table" "public_route_table" {
 vpc_id = aws_vpc.vpc.id
 route {
