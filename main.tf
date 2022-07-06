0fc106bac547dd808a04490dd74be4f366ec2fbe
Chiderah <chiderah96@gmail.com>
2022-07-06 12:35:43 -0500

    set changes

--- main.tf
+++ main.tf
@@ -16,14 +16,12 @@ tags = {
 }
 #Deploy the private subnets
 resource "aws_subnet" "private_subnets" {
-for_each
-vpc_id
-cidr_block
+for_each    = var.private_subnets
+vpc_id      = aws_vpc.vpc.id
+cidr_block  = cidrsubnet(var.vpc_cidr, 8, each.value)
 availability_zone = tolist(data.aws_availability_zones.available.names)[
 each.value]
-= var.private_subnets
-= aws_vpc.vpc.id
-= cidrsubnet(var.vpc_cidr, 8, each.value)
+
 tags = {
     Name      = each.key
     Terraform = "true"
