9b2860ffd934c2e63548f0539964ebc515d162dd
Chiderah <chiderah96@gmail.com>
2022-07-06 12:32:34 -0500

    set changes

--- variables.tf
+++ variables.tf
@@ -9,11 +9,9 @@ variable "private_subnets" { default = {
 "private_subnet_1" = 1
     "private_subnet_2" = 2
     "private_subnet_3" = 3
-    }
-}
+}}
 variable "public_subnets" { default = {
     "public_subnet_1" = 1
     "public_subnet_2" = 2
     "public_subnet_3" = 3
-    }
-}
+}}
